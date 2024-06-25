1526690a358dd150bece94e0a921b6bfdc37326e
Hikanyan <hikanyan0131@gmail.com>
2024-06-26 02:12:14 +0900

    [Update]

--- Assets/HikanyanLaboratory/Script/TitleScene/TitleController.cs
+++ Assets/HikanyanLaboratory/Script/TitleScene/TitleController.cs
@@ -1,7 +1,27 @@
-﻿namespace HikanyanLaboratory.Script.TitleScene
+﻿using UnityEditor;
+
+namespace HikanyanLaboratory.Script.TitleScene
 {
     public class TitleController
     {
-        
+        private readonly TitlePresenter _presenter;
+
+        public TitleController(TitlePresenter presenter)
+        {
+            _presenter = presenter;
+        }
+
+        public void StartGame()
+        {
+        }
+
+        void Quit()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false; //ゲームプレイ終了
+#else
+        Application.Quit();//ゲームプレイ終了
+#endif
+        }
     }
 }
--- Assets/HikanyanLaboratory/Script/TitleScene/TitleUIManager.cs
+++ Assets/HikanyanLaboratory/Script/TitleScene/TitleUIManager.cs
@@ -1,36 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using UnityEditor;
 
 namespace HikanyanLaboratory.Script.TitleScene
 {
     public class TitleUIManager : MonoBehaviour
     {
+        [Header("Canvas")] 
         [SerializeField] private GameObject _backgroundCanvas;
         [SerializeField] private GameObject _titleUICanvas;
         [SerializeField] private GameObject _titleMenuCanvas;
+        [SerializeField] private GameObject _optionCanvas;
+        [SerializeField] private GameObject _LoginCanvas;
+        [SerializeField] private GameObject _debugCanvas;
 
-        
+        [Header("Button")]
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _optionButton;
+        [SerializeField] private Button _exitButton;
+
+
         public void ShowTitleMenu()
         {
             _backgroundCanvas.SetActive(false);
             _titleUICanvas.SetActive(false);
             _titleMenuCanvas.SetActive(true);
         }
-        
+
         public void ShowTitleUI()
         {
             _backgroundCanvas.SetActive(false);
             _titleUICanvas.SetActive(true);
             _titleMenuCanvas.SetActive(false);
         }
-        
+
         public void ShowBackground()
         {
             _backgroundCanvas.SetActive(true);
             _titleUICanvas.SetActive(false);
             _titleMenuCanvas.SetActive(false);
         }
-        
+
         public void HideAll()
         {
             _backgroundCanvas.SetActive(false);
