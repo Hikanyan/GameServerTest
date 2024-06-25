4a5fb064a1e6608f34fa6d43c3661d589de6f9b8
Hikanyan <hikanyan0131@gmail.com>
2024-06-26 00:00:51 +0900

    [Update]

--- Assets/HikanyanLaboratory/Script/LifeTimeScope/TitleLifeTimeScope.cs
+++ Assets/HikanyanLaboratory/Script/LifeTimeScope/TitleLifeTimeScope.cs
@@ -1,7 +1,16 @@
-﻿namespace HikanyanLaboratory.Script.LifeTimeScope
+﻿using HikanyanLaboratory.Script.TitleScene;
+using VContainer;
+using VContainer.Unity;
+
+namespace HikanyanLaboratory.Script.LifeTimeScope
 {
-    public class TitleLifeTimeScope
+    public class TitleLifeTimeScope　: LifetimeScope
     {
-        
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<TitleController>(Lifetime.Singleton);
+            builder.Register<TitlePresenter>(Lifetime.Singleton);
+            builder.Register<TitleView>(Lifetime.Singleton);
+        }
     }
 }
