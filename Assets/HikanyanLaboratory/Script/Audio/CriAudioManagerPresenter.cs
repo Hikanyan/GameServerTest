1ffa87491a6194a18171473082846f0f41f3ee69
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-07-09 16:45:22 +0900

    [Fix] NameSpaceを修正

--- Assets/HikanyanLaboratory/Script/Audio/CriAudioManagerPresenter.cs
+++ Assets/HikanyanLaboratory/Script/Audio/CriAudioManagerPresenter.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
+using VContainer.Unity;
 
 namespace HikanyanLaboratory.Audio
 {
-    public class CriAudioManagerPresenter
+    public class CriAudioManagerPresenter:IInitializable
     {
-        
+        private readonly CriAudioManager _criAudioManager;
+
+        public CriAudioManagerPresenter(CriAudioManager criAudioManager)
+        {
+            _criAudioManager = criAudioManager;
+        }
+
+        public void Initialize()
+        {
+            Debug.Log("CriAudioManagerPresenter Initialize");
+        }
     }
 }
