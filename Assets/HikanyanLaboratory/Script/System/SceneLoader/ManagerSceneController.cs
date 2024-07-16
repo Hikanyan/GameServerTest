1577c23db982c0384d1e9a2ee81017b2c733b875
Hikanyan <hikanyan0131@gmail.com>
2024-07-16 23:34:03 +0900

    [Update]

--- Assets/HikanyanLaboratory/Script/System/SceneLoader/ManagerSceneController.cs
+++ Assets/HikanyanLaboratory/Script/System/SceneLoader/ManagerSceneController.cs
@@ -6,46 +6,83 @@ namespace HikanyanLaboratory.System
 {
     public class ManagerSceneController
     {
+        private Scene _currentScene;
         private readonly SceneLoader _sceneLoader;
-        private readonly Scene _currentScene;
-        private readonly Scene _nextScene;
+        private readonly string _managerScene = "ManagerScene";
 
+        /// <summary>
+        /// 最初にManagerSceneを読み込む
+        /// </summary>
+        /// <param name="sceneLoader"></param>
         [Inject]
         public ManagerSceneController(SceneLoader sceneLoader)
         {
             _sceneLoader = sceneLoader;
+            LoadManagerScene().Forget();
         }
 
+        /// <summary>
+        /// 現在のシーンをUnloadしてから新しいシーンをLoadする
+        /// </summary>
+        /// <param name="sceneName"> 新しいシーンの名前 </param>
         public async UniTaskVoid ChangeScene(string sceneName)
         {
             await CurrentSceneUnload();
             await _sceneLoader.LoadSceneAsync(sceneName);
+            _currentScene.name = sceneName;
         }
 
+        /// <summary>
+        /// TitleSceneを読み込む
+        /// </summary>
         public async UniTaskVoid LoadTitleScene()
         {
             await CurrentSceneUnload();
             await _sceneLoader.LoadSceneAsync("TitleScene");
+            _currentScene.name = "TitleScene";
         }
 
+        /// <summary>
+        /// GameSceneを読み込む
+        /// </summary>
         public async UniTaskVoid LoadGameScene()
         {
             await CurrentSceneUnload();
             await _sceneLoader.LoadSceneAsync("InGameScene");
+            _currentScene.name = "InGameScene";
         }
 
+        /// <summary>
+        /// ResultSceneを読み込む
+        /// </summary>
         public async UniTaskVoid LoadResultScene()
         {
             await CurrentSceneUnload();
             await _sceneLoader.LoadSceneAsync("ResultScene");
+            _currentScene.name = "ResultScene";
         }
 
-        public async UniTaskVoid LoadManagerScene()
+        /// <summary>
+        /// LobbySceneを読み込む
+        /// </summary>
+        public async UniTaskVoid LoadLobbyScene()
         {
-            
-            await _sceneLoader.LoadSceneAsync("ManagerScene");
+            await CurrentSceneUnload();
+            await _sceneLoader.LoadSceneAsync("LobbyScene");
+            _currentScene.name = "LobbyScene";
         }
 
+        /// <summary>
+        /// UnloadしないでManagerSceneを読み込む
+        /// </summary>
+        private async UniTaskVoid LoadManagerScene()
+        {
+            await _sceneLoader.LoadSceneAsync(_managerScene);
+        }
+
+        /// <summary>
+        ///　現在のシーンをUnloadする
+        /// </summary>
         private async UniTask CurrentSceneUnload()
         {
             await _sceneLoader.UnloadSceneAsync(_currentScene.name);
