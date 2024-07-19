391a5a202f8c1eb8ad6b244d3eb1f725415c68a0
Hikanyan <hikanyan0131@gmail.com>
2024-07-19 14:49:46 +0900

    [Add] Shader フェードUIPrefab

--- Assets/HikanyanLaboratory/Script/System/SceneLoader/BasicFadeStrategy.cs
+++ Assets/HikanyanLaboratory/Script/System/SceneLoader/BasicFadeStrategy.cs
@@ -5,35 +5,25 @@ using UnityEngine;
 
 namespace HikanyanLaboratory.System
 {
+    [Serializable]
     public class BasicFadeStrategy : IFadeStrategy
     {
-        private readonly Material _fadeMaterial;
-        private readonly float _fadeDuration;
-        private float _cutoutRange;
-
         private static readonly int Range1 = Shader.PropertyToID("_Range");
 
-        public BasicFadeStrategy(Material fadeMaterial, float fadeDuration, float cutoutRange)
+        public async UniTask FadeOut(Material fadeMaterial, float fadeDuration, float cutoutRange, Ease ease)
         {
-            _fadeMaterial = fadeMaterial;
-            _fadeDuration = fadeDuration;
-            _cutoutRange = cutoutRange;
+            DOTween.To(() => cutoutRange, x => cutoutRange = x, 1, fadeDuration)
+                .OnUpdate(() => fadeMaterial.SetFloat(Range1, 1 - cutoutRange))
+                .SetEase(ease);
+            await UniTask.Delay(TimeSpan.FromSeconds(fadeDuration));
         }
 
-        public async UniTask FadeOut()
+        public async UniTask FadeIn(Material fadeMaterial, float fadeDuration, float cutoutRange, Ease ease)
         {
-            DOTween.To(() => _cutoutRange, x => _cutoutRange = x, 1, _fadeDuration)
-                .OnUpdate(() => _fadeMaterial.SetFloat(Range1, 1 - _cutoutRange))
-                .SetEase(Ease.Linear);
-            await UniTask.Delay(TimeSpan.FromSeconds(_fadeDuration));
-        }
-
-        public async UniTask FadeIn()
-        {
-            DOTween.To(() => _cutoutRange, x => _cutoutRange = x, 0, _fadeDuration)
-                .OnUpdate(() => _fadeMaterial.SetFloat(Range1, 1 - _cutoutRange))
-                .SetEase(Ease.Linear);
-            await UniTask.Delay(TimeSpan.FromSeconds(_fadeDuration));
+            DOTween.To(() => cutoutRange, x => cutoutRange = x, 0, fadeDuration)
+                .OnUpdate(() => fadeMaterial.SetFloat(Range1, 1 - cutoutRange))
+                .SetEase(ease);
+            await UniTask.Delay(TimeSpan.FromSeconds(fadeDuration));
         }
     }
 }
